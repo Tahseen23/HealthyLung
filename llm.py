48800448d316763791d800e8228e4a1441c737d2
Tahseen Kausar <tahseenkausar24@gmail.com>
2024-07-12 18:04:16 +0530

    api update

--- llm.py
+++ llm.py
@@ -4,6 +4,7 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import  ChatPromptTemplate
 from langchain.chains import HypotheticalDocumentEmbedder
+import streamlit as st
 
 
 from langchain.vectorstores import Pinecone
@@ -22,7 +23,8 @@ key=os.environ.get('api_key')
 model=HuggingFaceEndpoint(repo_id=repo_id,huggingfacehub_api_token=key,add_to_git_credential=True)
 embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 index_name='lung'
-os.environ['PINECONE_API_KEY'] = os.environ.get('pineapi')
+# os.environ['PINECONE_API_KEY'] = os.environ.get('pineapi')
+auth=st.secrets['pine']
 
 hyde_embeddings=HypotheticalDocumentEmbedder.from_llm(model,
                                                     embeddings,
